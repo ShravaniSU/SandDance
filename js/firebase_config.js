822ea37b5f06988d0c9eef2d724e8416d83d230f
Shravani Urankar <shravanisurankar@gmail.com>
2019-12-08 16:51:58 +0530

    first commit

---
+++ js/firebase_config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD43FaXuaODtaYPaIHvX5rLFlU9TPT_Qkc",
+    authDomain: "sanddance-3aca7.firebaseapp.com",
+    databaseURL: "https://sanddance-3aca7.firebaseio.com",
+    projectId: "sanddance-3aca7",
+    storageBucket: "sanddance-3aca7.appspot.com",
+    messagingSenderId: "164284429414",
+    appId: "1:164284429414:web:36ca1848f6896cc15529a7",
+    measurementId: "G-3G4GMM3CV5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
